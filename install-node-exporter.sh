930c8074a980fdc8e000532882513ddd4ac63515
Mehetre, Nikhil (EXTERN: Expleo GmbH) <extern.nikhil.mehetre@volkswagen.de>
2022-09-16 08:04:07 +0530

    .sh

---
+++ install-node-exporter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+tar -xvf node_exporter-1.3.1.linux-amd64.tar.gz
+sudo mv -v node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
+sudo chown root:root /usr/local/bin/node_exporter
+node_exporter --version
+sudo echo '[Unit]
+Description=Prometheus exporter for machine metrics
+
+[Service]
+Restart=always
+User=root
+ExecStart=/usr/local/bin/node_exporter
+ExecReload=/bin/kill -HUP $MAINPID
+TimeoutStopSec=20s
+SendSIGKILL=no
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/node-exporter.service
+sudo systemctl daemon-reload
+sudo systemctl start node-exporter.service
+sudo systemctl enable node-exporter.service
+sudo systemctl status node-exporter.service
