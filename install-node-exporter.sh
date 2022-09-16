1387b07be8bff89a3bd748003342debd3942550e
Mehetre, Nikhil (EXTERN: Expleo GmbH) <extern.nikhil.mehetre@volkswagen.de>
2022-09-16 08:27:35 +0530

    11

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
-tar -xvf node_exporter-1.3.1.linux-amd64.tar.gz
-sudo mv -v node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
-sudo chown root:root /usr/local/bin/node_exporter
+#wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+#tar -xvf node_exporter-1.3.1.linux-amd64.tar.gz
+#sudo mv -v node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/
+chown root:root /opt/nodeexporter
 node_exporter --version
-sudo echo '[Unit]
+echo '[Unit]
 Description=Prometheus exporter for machine metrics
 
 [Service]
 Restart=always
 User=root
-ExecStart=/usr/local/bin/node_exporter
+ExecStart=/opt/nodeexporter/
 ExecReload=/bin/kill -HUP $MAINPID
 TimeoutStopSec=20s
 SendSIGKILL=no
@@ -21,3 +21,4 @@ sudo systemctl daemon-reload
 sudo systemctl start node-exporter.service
 sudo systemctl enable node-exporter.service
 sudo systemctl status node-exporter.service
+
